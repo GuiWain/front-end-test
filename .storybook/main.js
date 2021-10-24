6c2aef1318b6cbbacb779f12cfb2a7c9f6081f9c
Guilherme Mariano <cmariano.gui@gmail.com>
2021-10-24 16:59:36 -0300

    feat: add templates to stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   "stories": [
     "../src/components/**/stories.@(js|jsx|ts|tsx)",
+    "../src/templates/**/stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
